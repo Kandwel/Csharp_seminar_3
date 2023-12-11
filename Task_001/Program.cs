daca62aa7dc142a0cc1fa447eeadcc40bb232611
unknown <C:\Users\GMS\AppData\Roaming\The Bat!>
2023-12-11 15:23:12 +0700

    Решение задания 1

--- Task_001/Program.cs
+++ Task_001/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 1: Напишите программу, которая бесконечно
+//запрашивает целые числа с консоли. Программа
+//завершается при вводе символа ‘q’ или при вводе
+//числа, сумма цифр которого четная.
+
+int count = 0, sum = 0, num;
+string input; 
+
+System.Console.WriteLine("Задание 1");
+System.Console.WriteLine("Программа бесконечно запрашивает числа.");
+System.Console.WriteLine("Если пользователь вводит q или сумма цифр числа чётное,");
+System.Console.WriteLine("то программа завершает работу.");
+Console.WriteLine();
+
+InfinityNumbers();
+
+void InfinityNumbers()
+{
+    System.Console.WriteLine("Введите любое число либо введите 'q' для выхода: ");
+    input = Console.ReadLine();
+    if (input != "q" || input != "Q")
+    {
+        sum = 0;
+        if (int.TryParse(input, out num))
+        {
+            while (num > 0)
+            {
+                sum = sum + (num % 10);
+                num = num / 10;
+            }
+
+            if (sum % 2 == 0)
+            {
+                System.Console.WriteLine("Сумма цифр чётная, программа завершена!");
+            }
+            else
+                InfinityNumbers();
+        }
+    }
+    else if (input == "q" || input == "Q")
+    {
+        System.Console.WriteLine("Вы ввели 'q', программа завершена!");
+    }
+    
+}
